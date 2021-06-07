08fa1df5793e4112b59c6045ab1446afbf0bd4e6
jemouraa <jecruz12@gmail.com>
2021-06-07 15:49:33 -0300

    atualizando o fork

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common'
-
 import { AppRoutingModule } from './app-routing.module';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 
 import { AppComponent } from './app.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { EntrarComponent } from './entrar/entrar.component';
 import { MenuComponent } from './menu/menu.component';
-import { ParceirosComponent } from './parceiros/parceiros.component';
-import { RodapeComponent } from './rodape/rodape.component';
-import { EntrarComponent } from './entrar/entrar.component';
-import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { ComponentInicioComponent } from './component-inicio/component-inicio.component';
-import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule } from '@angular/forms';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 
@@ -26,9 +18,8 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     MenuComponent,
     CadastrarComponent,
-    ComponentInicioComponent,
-    InicioComponent,
     LoginComponent,
+    InicioComponent
     
     
     
